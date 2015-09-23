d7a6d6bcf49d477d17e9939fc0a70f1f890435c6
Timer91 <marais.jeremie@gmail.com>
2015-09-23 10:50:45 +0200

    Files: drop directories on Chrome

--- files/drop.js
+++ files/drop.js
@@ -1,13 +1,38 @@
 (function() {
 
+function isFileOrDirectory( item ) {
+	if ( item.isFile ) {
+		item.file( function( file ) {
+			playerAPI.addFile( file );
+		});
+	} else if ( item.isDirectory ) {
+		var dirReader = item.createReader();
+		dirReader.readEntries( function( entries ) {
+			$.each( entries, function( key, entry ) {
+				isFileOrDirectory( entry );
+			});
+		});
+	}
+}
+
 $( document.body )
 	.on( "dragover", false )
 	.on( "drop", function( e ) {
-		e = e.originalEvent;
-
-		$.each( e.dataTransfer.files, function() {
-			playerAPI.addFile( this );
-		});
+		if ( ( data = e.originalEvent.dataTransfer ) ) {
+			// Drop folders only on Chrome
+			if ( data.items ) {
+				$.each( data.items, function( key, item ) {
+					if ( ( item = item.webkitGetAsEntry() ) ) {
+						isFileOrDirectory( item );
+ 					}
+				});
+			}
+			else if ( data.files ) {
+				$.each( data.files, function( key, file ) {
+					playerAPI.addFile( files );
+				});
+			}
+		}
 
 		return false;
 	})
