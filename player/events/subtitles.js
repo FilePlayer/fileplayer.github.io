f7feb5186491002d91fa6e6db36dfb9043abe881
Thomas Tortorini <thomastortorini@gmail.com>
2015-12-16 18:16:38 +0100

    Subtitles: more shortcut for delaying subtitles (shift/alt/ctrl+g/h)

--- player/events/subtitles.js
+++ player/events/subtitles.js
@@ -5,10 +5,20 @@ dom.jqPlayerSubtitlesToggle
 		.click( api.subtitles.toggle )
 ;
 
+function del( time ) {
+	return api.subtitles.delay.bind( null, time );
+}
+
 // Synchronisation's subtitles keyboard.
 api.keyboard
-	.shortcut( "g", api.subtitles.delay.bind( null, "-=.05" ) )
-	.shortcut( "h", api.subtitles.delay.bind( null, "+=.05" ) )
+	.shortcut( "g",       del( "-=.05" ) )
+	.shortcut( "h",       del( "+=.05" ) )
+	.shortcut( "shift+g", del( "-=.25" ) )
+	.shortcut( "shift+h", del( "+=.25" ) )
+	.shortcut( "alt+g",   del( "-=1" ) )
+	.shortcut( "alt+h",   del( "+=1" ) )
+	.shortcut( "ctrl+g",  del( "-=5" ) )
+	.shortcut( "ctrl+h",  del( "+=5" ) )
 ;
 
 })();
