04eda29b689196d3483e54609fd15faf571b60a4
Misty418 <melanieducani@gmail.com>
2016-01-07 03:25:36 +0100

    API+Audio: introduction of the visualisation's system

--- api/audio.js
+++ api/audio.js
@@ -1,10 +1,67 @@
 (function() {
 
 var
-	ctx = new AudioContext(),
-	src = ctx.createMediaElementSource( dom.jqPlayerVideo[ 0 ] )
+	requestId,
+	visu = {},
+	nbVisu = 0,
+	selectedVisu = $.noop,
+	canvas = dom.jqPlayerCanvas[ 0 ],
+	ctxCanvas = canvas.getContext( '2d', { alpha: false } ),
+	ctxAudio = new AudioContext(),
+	analyser = ctxAudio.createAnalyser(),
+	src = ctxAudio.createMediaElementSource( dom.jqPlayerVideo[ 0 ] ),
+	visuEnable = false
 ;
 
-src.connect( ctx.destination );
+src.connect( analyser );
+analyser.connect( ctxAudio.destination );
+
+api.audio = that = {
+	addVisu: function( name, fn ) {
+		visu[ name ] = fn;
+		return that;
+	},
+	resize: function() {
+		canvas.width = api.video.elementWidth;
+		canvas.height = api.video.elementHeight;
+		return that;
+	},
+	selectVisu: function( name ) {
+		selectedVisu = visu[ name ] || $.noop;
+		return that;
+	},
+	visuOn: function() {
+		var info = {
+			width: canvas.width,
+			height: canvas.height,
+			ctxCanvas: ctxCanvas,
+			analyser: analyser,
+			data: new Uint8Array( 2048 ) // TODO (analyser.frequencyBinCount)
+		};
+		function frame( timestamp ) {
+			ctxCanvas.clearRect( 0, 0, canvas.width, canvas.height );
+			selectedVisu( info );
+			requestId = requestAnimationFrame( frame );
+		}
+		if ( !visuEnable ) {
+			requestId = requestAnimationFrame( frame );
+			visuEnable = true;
+		}
+		return that;
+	},
+	visuOff: function() {
+		if ( visuEnable ) {
+			cancelAnimationFrame( requestId );
+			visuEnable = false;
+		}
+		return that;
+	},
+	visuToggle: function( b ) {
+		if ( !arguments.length ) {
+			b = !visuEnable;
+		}
+		return b ? that.visuOn() : that.visuOff();
+	}
+};
 
 })();
