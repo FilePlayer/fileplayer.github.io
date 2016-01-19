5a88819dce57d7b8469c7a5b445d4e24989771f9
Thomas Tortorini <thomastortorini@gmail.com>
2016-01-19 05:32:02 +0100

    Build: setup a build bash file

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Build the big `style.css` file
+# Here we are removing temporarely the Jekyll's frontmatter of the SCSS file.
+cd css;
+cp style.scss _style.scss;
+tail +3 _style.scss > style.scss;
+sass style.scss style.css;
+rm style.scss;
+mv _style.scss style.scss;
